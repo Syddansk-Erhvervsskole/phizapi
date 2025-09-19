a69045a667eae6f99bd4e5ef98ea7fa0bf3af06e
Rasmus Andersen <ra@house4it.com>
2025-09-19 08:34:48 +0200

    Encryption service added to handle passwords

---
+++ phizapi/Services/EncryptionService.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace phizapi.Services
+{
+    public class EncryptionService
+    {
+        private readonly IConfiguration _config;
+        public EncryptionService(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string Sha256(string input)
+        {
+            string key = _config["Sha256:Key"];  // hardcoded
+            string combined = key + input;
+
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(combined);
+                byte[] hash = sha256.ComputeHash(bytes);
+                return Convert.ToHexString(hash).ToLower();
+            }
+        }
+
+    }
+}
